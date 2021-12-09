385d053b4f3452e7042e6f1c15a74cfabe54b9a3
sampet8 <Samuel.Petras@studentstc.sk>
2021-12-09 23:08:13 +0100

    final update

--- books final/methods.ts
+++
@@ -1,6 +0,0 @@
-import { Book_Short, Book } from './books_data';
-import * as fs from "fs";
-
-export function save(books: Book[]){
-    fs.writeFileSync("books.json", (JSON.stringify(books, null, 2)))
-}
